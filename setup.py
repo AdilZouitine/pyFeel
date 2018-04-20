3c672a1df4a4e7cbf60fae1fd157e0eab98c2b0a
Unknown <adilzouitinegm@gmail.com>
2018-04-20 23:52:29 +0200

    Add github url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pyFeel
 setup(name='pyFeel',
       version=pyFeel.__version__,
       description='French sentimental analysis',
-      url='',
+      url='https://github.com/AdilZouitine/pyFeel',
       author='Adil Zouitine',
       author_email='adilzouitine@gmail.com',
       license='MIT',
