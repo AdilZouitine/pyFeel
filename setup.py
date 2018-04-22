30665c86bb2abf539e08b2be567a657471bec4ba
Unknown <adilzouitinegm@gmail.com>
2018-04-22 18:24:47 +0200

    description change

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import pyFeel
 
 setup(name='pyFeel',
       version=pyFeel.__version__,
-      description='French sentimental analysis',
+      description='French sentiment analysis',
       url='https://github.com/AdilZouitine/pyFeel',
       author='Adil Zouitine',
       author_email='adilzouitine@gmail.com',
